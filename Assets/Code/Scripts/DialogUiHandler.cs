ee594722fa8a36f9a6b15f28943c9463889158df
Mohammed Musthafa <mohamedmusthafa3679@gmail.com>
2024-05-22 18:30:15 +0400

    Removed Coroutine From Dialouge Ui Handler

--- Assets/Code/Scripts/DialogUiHandler.cs
+++ Assets/Code/Scripts/DialogUiHandler.cs
@@ -10,18 +10,43 @@ public class DialogUiHandler : SingletonBehavior<DialogUiHandler>
 
     public void ShowDialogue(DialogueItemSO dialogueItem)
     {
-        StartCoroutine(DisplayDialogueFor(dialogueItem.dialogueText, dialogueItem.duration));
+        //StartCoroutine(DisplayDialogueFor(dialogueItem.dialogueText, dialogueItem.duration));
+        SetActiveDialogueObject(true);
+        SetDialogue(string.Empty);
+        DisplayDialogueFor(dialogueItem.dialogueText, dialogueItem.duration);
     }
 
-    IEnumerator DisplayDialogueFor(string dialogue, float duration)
+    //IEnumerator DisplayDialogueFor(string dialogue, float duration)
+    //{
+    //    SetActiveDialogueObject(true);
+    //    SetDialogue(string.Empty);
+    //    dialogueHolder.DOText(dialogue, dialogue.Length * typeSpeed).SetEase(Ease.Linear);
+
+    //    yield return new WaitForSeconds(duration);
+    //    SetActiveDialogueObject(false);
+    //}
+
+
+
+    private void DisplayDialogueFor(string dialogue, float duration)
     {
-        dialogueHolder.gameObject.SetActive(true);
-        dialogueHolder.text = "";
-        dialogueHolder.DOText(dialogue, dialogue.Length * typeSpeed).SetEase(Ease.Linear);
+        float textDuration = (dialogue.Length / duration) * typeSpeed;
 
-        yield return new WaitForSeconds(duration);
-        dialogueHolder.gameObject.SetActive(false);
-
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(dialogueHolder.DOText(dialogue, textDuration).SetEase(Ease.Linear));
+        sequence.AppendInterval(duration); // Add delay after text animation completes
+        sequence.OnComplete(() => {
+            SetActiveDialogueObject(false);
+        });
     }
 
+    private void SetActiveDialogueObject(bool active)
+    {
+        dialogueHolder.gameObject.SetActive(active);
+    }
+
+    private void SetDialogue(string text)
+    {
+        dialogueHolder.text = text;
+    }
 }
